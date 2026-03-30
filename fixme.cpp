be0b850586fa24b00fb0a7fecdcfc7e1456bdf43
dunuup16 <dunuup@yandex.ru>
2026-03-30 16:05:56 +0300

    Create fixme.cpp
    Добавлен начальный файл для лабораторной работы №4

---
+++ fixme.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // TODO: Fix this code
+    cout << "Hello, World!" << endl;
+    
+    int a = 5;
+    int b = 10;
+    int result = a + b;
+    
+    cout << "Result: " << result << endl;
+    
+    return 0;
+}
+Add initial fixme.cpp
